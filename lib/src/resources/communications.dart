714c3a079f819676dfefed2215eb219b3361b318
mpily <mboghobill16@gmail.com>
2021-07-08 11:44:24 +0300

    updated clases for communication

--- lib/src/resources/communications.dart
+++ lib/src/resources/communications.dart
@@ -8,22 +8,7 @@ import 'dart:convert';
 
 import 'dart:io';
 
-//Declaring non-nullable variable udp
-late RawDatagramSocket udp;
-
-
-//IN the event that data is received....
-
-void onData(RawSocketEvent event) {
-  print("Socket event.");
-  if (event == RawSocketEvent.read) {
-    Datagram? rcv = udp.receive();
-    print("Received data: " + ascii.decode(rcv!.data));
-  }
-}
-
 void main() async {
-
   packetHandler test = packetHandler();
   await test.initializeIp(57194);
   await Future.delayed(Duration(seconds: 1));
@@ -46,11 +31,7 @@ class packetHandler {
     }
   }
 
-  final int port = 57194;
-  udp = await RawDatagramSocket.bind(InternetAddress.anyIPv4, port);
-
-
-  packetHandler() {}
+  packetHandler();
 
   Future<void> initializeIp(int number) async {
     this.socketConnection =
@@ -58,35 +39,25 @@ class packetHandler {
     print('got here $this.socketConnection');
     this.portNumber = number;
 
-    socketConnection.listen(this.onData);
+    this.socketConnection.listen(this.onData);
     print("The socket is connected to: ${socketConnection.address}:" +
-        socketConnection.port.toString());
+        this.socketConnection.port.toString());
     List<NetworkInterface> ni = await NetworkInterface.list();
     print("The internet address is: ${ni[0].addresses[0].address}");
 
     print(
-        "Enter the IP address of the socket to connect to (The same port {$portNumber} will be used): ");
-    destIpString = stdin.readLineSync();
+        "Enter the IP address of the socket to connect to (The same port ${this.portNumber} will be used): ");
+    this.destIpString = stdin.readLineSync();
   }
 
   Future<void> sendData() async {
-    print("Enter the string to be sent to [$destIpString:$portNumber]: ");
-
-  print("The socket is connected to: ${udp.address}:" + udp.port.toString());
-  List<NetworkInterface> ni = await NetworkInterface.list();
-  print("The internet address is: ${ni[0].addresses[0].address}");
-
-  print(
-      "Enter the IP address of the socket to connect to (The same port {$port} will be used): ");
-  String? destIpString = stdin.readLineSync();
-
-  while (true) {
-    print("Enter the string to be sent to [$destIpString:$port]: ");
+    print(
+        "Enter the string to be sent to [${this.destIpString}:${this.portNumber}]: ");
 
     String? stringToSend = stdin.readLineSync();
     List<int> buffer = ascii.encode(stringToSend!);
-    InternetAddress destAddress = InternetAddress(destIpString!);
-    socketConnection.send(buffer, destAddress, portNumber);
+    InternetAddress destAddress = InternetAddress(this.destIpString!);
+    socketConnection.send(buffer, destAddress, this.portNumber);
     print("The string has been sent.");
     //We need a delay to allow the main thread with input to hand over control of the terminal
     //  to the listen thread so that it can output to console
