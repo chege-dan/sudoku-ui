fa5fbe726c8b5d2c09da749b17d70581a41cb252
mpily <mboghobill16@gmail.com>
2021-07-09 10:25:14 +0300

    updated engine to have the event handler for communication

--- lib/src/resources/engine.dart
+++ lib/src/resources/engine.dart
@@ -2,8 +2,14 @@ import 'package:sudoku_crypto/src/models/sudoku.dart';
 import 'package:sudoku_crypto/src/models/database.dart';
 import 'package:sudoku_crypto/src/blocs/current_sudoku.dart';
 import 'package:sudoku_crypto/src/blocs/database_state.dart';
-
-
+import 'package:sudoku_crypto/src/resources/communications.dart';
+/*
+  sending data using the packet handler class:
+  test.send
+*/ */
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io'
 //Main class that will be globally accessible
 class AppEngine {
   //To hold all the database data
@@ -18,10 +24,29 @@ class AppEngine {
   DatabaseStateBLoC databaseStateBLoC = DatabaseStateBLoC();
   //To communicate with the server
   //TODO: Make the communications class with all the required function members
+  late packetHandler test;
+  void onData(RawSocketEvent event) {
+    // and event handler for when a packet is received
+    print("socket event");
+    if (event == RawSocketEvent.read) {
+      Datagram? rcv = test.socketConnection.receive();
+      print("Received data" + ascii.decode(rcv!.data));
+      packet_splitter pcktreceived = packet_splitter(ascii.decode(rcv!.data));
+      if (pcktreceived.type == "0") {
+        //the MCU is updating the sudoku
+        //TODO CONFIRM SUDOKUS ARE THE SAME ; ACCESS A STRING REPRESENTATIOON OF THE SUDOKU
+        //RECEIVED USING COMMAND pcktreceived.sudoku;
+
+      } else if(pcktreceived.type == "2"){
+        //someone has completed the sudoku and won
+        //TODO Update the database
+        //access the person who solved with pcktreceived.solver the old sudoku id is pckreceive.id
+      }
+    }
+  }
 
   AppEngine() {
     //Initializer for the class
-
   }
 
   void dispose() {
@@ -31,14 +56,12 @@ class AppEngine {
   }
 
   bool checkSudokuComplete(Sudoku sudoku) {
-
     //TODO: Write code for checking if the sudoku is complete (all numbers present)
 
     return false;
   }
 
   bool checkSudokuCorrectness(Sudoku sudoku) {
-
     //TODO: Write code for checking if complete sudoku is correct
 
     return false;
